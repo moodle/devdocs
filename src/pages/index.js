e12eb6c9e2b6587aab197e179f624cba5feedc9f
Andrew Nicols <andrew@nicols.co.uk>
2022-04-05 22:05:08 +0800

    Improve site description

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome to the Moodle Developer Resource site`}
+      description="The Work-in-progress (not-yet official) source of Developer Resources for Moodle Developers.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
