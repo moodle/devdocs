e11e206429e58a4cd79028cc3eb39270f9443ef7
Andrew Nicols <andrew@nicols.co.uk>
2022-04-19 22:14:42 +0800

    [repo] Correct path for mdk migration

--- migratedPages.js
+++ migratedPages.js
@@ -18,40 +18,40 @@ const path = require('path');
 const fs = require('fs');
 
 const obsoleteDocs = [
-    "MDLQA-features",
-    "Setting_up_Eclipse",
-    "Setting_up_Netbeans",
+    'MDLQA-features',
+    'Setting_up_Eclipse',
+    'Setting_up_Netbeans',
 ];
 
 /**
  * A list of documents which have been migrated with their source and destination paths shown.
  */
 const migratedDocs = {
-    Access_API: "/docs/apis/access.md",
-    Activity_modules: "/docs/apis/plugintypes/mod.md",
-    CodeSniffer: "/general/development/tools/phpcs",
-    Coding_style: "/general/development/policies/codingstyle/index.md",
-    Communication_Between_Components: "/general/development/policies/component-communication/index.md",
-    Core_APIs: "/docs/apis.md",
-    Developer_meeting_February_2022: "/general/community/meetings/202202.md",
-    Developer_meetings: "/general/community/meetings.md",
-    Integration_Review: "/general/development/process/integration-review.md",
-    Mission: "/general/community/mission.md",
-    "Moodle_4.0_release_notes": "/docs/release-notes.md",
-    Moodle_App: "/docs/guides/moodleapp/coding-style.md",
-    Moodle_App_Coding_Style: "/docs/guides/moodleapp/coding-style.md",
-    Moodle_App_Overview: "/docs/guides/moodleapp/overview.md",
-    Moodle_Development_kit: "/general/development/tools/mdk",
-    Moodle_research: "/general/community/research.md",
-    Overview: "/general/community/intro.md",
-    Peer_reviewing: "/general/development/process/peer-review.md",
-    Process: "/general/development/process.md",
-    QA_testing: "/general/development/process/testing/qa.md",
-    Roadmap: "/general/community/roadmap.md",
-    Testing: "/general/development/process/testing.md",
-    Testing_of_integrated_issues: "/general/development/process/testing/integrated-issues.md",
-    Tracker_introduction: "/general/development/tracker.md",
-    Tracker_tips: "/general/development/tracker/tips.md",
+    Access_API: '/docs/apis/access.md',
+    Activity_modules: '/docs/apis/plugintypes/mod.md',
+    CodeSniffer: '/general/development/tools/phpcs',
+    Coding_style: '/general/development/policies/codingstyle/index.md',
+    Communication_Between_Components: '/general/development/policies/component-communication/index.md',
+    Core_APIs: '/docs/apis.md',
+    Developer_meeting_February_2022: '/general/community/meetings/202202.md',
+    Developer_meetings: '/general/community/meetings.md',
+    Integration_Review: '/general/development/process/integration-review.md',
+    Mission: '/general/community/mission.md',
+    'Moodle_4.0_release_notes': '/docs/release-notes.md',
+    Moodle_App: '/docs/guides/moodleapp/coding-style.md',
+    Moodle_App_Coding_Style: '/docs/guides/moodleapp/coding-style.md',
+    Moodle_App_Overview: '/docs/guides/moodleapp/overview.md',
+    Moodle_Development_kit: '/general/development/tools/mdk.md',
+    Moodle_research: '/general/community/research.md',
+    Overview: '/general/community/intro.md',
+    Peer_reviewing: '/general/development/process/peer-review.md',
+    Process: '/general/development/process.md',
+    QA_testing: '/general/development/process/testing/qa.md',
+    Roadmap: '/general/community/roadmap.md',
+    Testing: '/general/development/process/testing.md',
+    Testing_of_integrated_issues: '/general/development/process/testing/integrated-issues.md',
+    Tracker_introduction: '/general/development/tracker.md',
+    Tracker_tips: '/general/development/tracker/tips.md',
 };
 
 const isObsolete = (legacyPath) => obsoleteDocs.indexOf(legacyPath) !== -1;
@@ -85,6 +85,7 @@ const getMigratedDoc = (legacyPath) => {
 
 const getAbsoluteDirectory = (relativePath) => {
     const absolutePath = path.join(process.env.PWD, relativePath);
+    /* eslint-disable-next-line no-restricted-properties */
     const pathStat = fs.statSync(absolutePath);
     if (pathStat.isFile()) {
         return path.dirname(absolutePath);
@@ -122,9 +123,8 @@ const getMigrationLink = (legacyPath, usedIn) => {
 
         if (relativeLink.startsWith('.')) {
             return relativeLink;
-        } else {
-            return `./${relativeLink}`;
         }
+        return `./${relativeLink}`;
     }
 
     if (replacementFile.endsWith('index.md')) {
