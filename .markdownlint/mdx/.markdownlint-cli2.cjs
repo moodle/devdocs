5555120dc6c11a3c7c2e214b2821473c9ae92fff
Andrew Nicols <andrew@nicols.co.uk>
2022-05-25 21:18:52 +0800

    [docs] Lint all mdx files
    MDX files are a subset of Markdown with JSX.
    
    As such there is necessarily inline HTML present in the document.

---
+++ .markdownlint/mdx/.markdownlint-cli2.cjs
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) Moodle Pty Ltd.
+ *
+ * Moodle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moodle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// For full details on all of these configuration settings:
+// https://github.com/DavidAnson/markdownlint#rules--aliases
+
+const config = require('../../.markdownlint-cli2.cjs');
+
+// MD033/no-inline-html - Inline HTML
+config.config.MD033 = false;
+
+// MD038/no-space-in-code Spaces inside code span elements
+config.config.MD038 = false;
+
+module.exports = config;
