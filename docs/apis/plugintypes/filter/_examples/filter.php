a9047f7ca3b1746c7d0976819f2910b29796541b
Andrew Nicols <andrew@nicols.co.uk>
2024-08-12 12:19:07 +0800

    docs: Update filter docs for MDL-82427

--- docs/apis/plugintypes/filter/_examples/filter.php
+++ docs/apis/plugintypes/filter/_examples/filter.php
@@ -1,4 +1,6 @@
-class filter_pluginname extends moodle_text_filter {
+namespace filter_pluginname;
+
+class text_filter extends \core_filters\text_filter {
     function filter(string $text, array $options = []) {
         // Return the modified text.
         return $text;
