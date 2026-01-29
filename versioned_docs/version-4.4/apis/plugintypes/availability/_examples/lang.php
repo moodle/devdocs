6601d648e1140285ddecd155c9a2ff72ae377e06
Mihail Geshoski <mihail@moodle.com>
2026-01-29 14:16:22 +0800

    [repo] Remove 4.1 and 4.4 docs

--- versioned_docs/version-4.4/apis/plugintypes/availability/_examples/lang.php
+++
@@ -1,3 +0,0 @@
-$string['description'] = 'Allow only students who belong to a group within a specified grouping.';
-$string['pluginname'] = 'Restriction by grouping';
-$string['title'] = 'Grouping';
