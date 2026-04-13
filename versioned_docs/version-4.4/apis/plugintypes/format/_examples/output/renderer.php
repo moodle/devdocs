878599b09e4eac93fa7081cd8ac6fa65f5b23f64
Andrew Nicols <andrew@nicols.co.uk>
2026-04-13 11:24:05 +0800

    [repo] Remove 4.4 docs

--- versioned_docs/version-4.4/apis/plugintypes/format/_examples/output/renderer.php
+++
@@ -1,9 +0,0 @@
-
-namespace format_pluginname\output;
-
-use core_courseformat\output\section_renderer;
-use moodle_page;
-
-class renderer extends section_renderer {
-    // Your renderer methods goes here.
-}
--- versioned_docs/version-4.4/apis/plugintypes/theme/_examples/lang.php
+++
@@ -1,3 +0,0 @@
-$string['pluginname'] = 'Boost';
-$string['choosereadme'] = 'Boost is a modern, highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
-$string['configtitle'] = 'Boost';
